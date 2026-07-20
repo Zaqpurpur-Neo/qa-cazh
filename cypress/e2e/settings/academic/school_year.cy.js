89e3a754ec3291939239941eb8c41fd4d0c16517
naufal <naufalsatrioputra@gmail.com>
2026-07-20 12:13:22 +0700

    restructure

--- cypress/e2e/settings/academic/school_year.cy.js
+++ cypress/e2e/settings/academic/school_year.cy.js
@@ -1,5 +1,12 @@
 describe('The School Year Page', () => {
     it('successfully loads', () => {
-        cy.visit('https://v3.cazh.id/setting/academic/school-year')
+        cy.visit('setting/academic/school-year')
+
+        cy.get('div[data-testid="type"]', {
+            timeout: 30000
+        }).
+            cy.get('div[data-testid="type"]', {
+                timeout: 30000
+            }).click()
     })
 })
